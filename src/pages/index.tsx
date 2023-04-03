9208fc425d60caf40e123a2498481435418a95e0
mala-narzi <shahin.behmand@gmail.com>
2023-04-03 22:40:36 +0430

    posting loading, taost

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,9 @@ import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
-import { type ChangeEvent, useState } from "react";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -23,12 +24,20 @@ const CreatePostWizard = () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
     },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
+    },
   });
 
   if (!user) return null;
 
   return (
-    <div className="flex w-full gap-3">
+    <div className="flex w-full items-center gap-3">
       <Image
         width={56}
         height={56}
@@ -40,11 +49,26 @@ const CreatePostWizard = () => {
         placeholder="Type some emojis"
         className="grow bg-transparent outline-none"
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         type="text"
         value={input}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex h-6 w-8 items-center justify-center">
+          {isPosting && <LoadingSpinner size={20} />}
+        </div>
+      )}
     </div>
   );
 };
