b769e48877113b823dd800dc0605139305542d1e
mala-narzi <shahin.behmand@gmail.com>
2023-04-07 18:57:22 +0430

    profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,21 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Toaster position="bottom-center" />
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <>
+      <Head>
+        <title>Twitter Emoji Clone</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ClerkProvider {...pageProps}>
+        <Toaster position="bottom-center" />
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </>
   );
 };
 
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,18 +1,10 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "../trpc";
-import type { User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profilePicture: user.profileImageUrl,
-  };
-};
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
